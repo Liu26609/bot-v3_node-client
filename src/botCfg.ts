6652f62010a580863a6206e33b36722ef4db7751
匿名用户 <8797550+ChuXuanD30@user.noreply.gitee.com>
2022-05-18 17:22:20 +0000

    删除文件 src/botCfg.ts

--- src/botCfg.ts
+++
@@ -1,9 +0,0 @@
-import { AvailableIntentsEventsEnum } from "qq-guild-bot";
-
-export default{
-    shards: [0, 1],
-    appID: '101983837', // ID
-    token: 'KRT0MyhsrJ4YGQXSyzAJiOJti58K982N', // token
-    intents: [AvailableIntentsEventsEnum.PUBLIC_GUILD_MESSAGES],    // 事件订阅,用于开启可接收的消息类型
-    sandbox: false
-}
