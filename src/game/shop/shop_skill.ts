d15616bf155a998dfda9a04a140b57c5e3f17dce
楚轩冬三十 <550620904@qq.com>
2022-05-23 21:40:27 +0800

    'autoUpdate'

---
+++ src/game/shop/shop_skill.ts
@@ -0,0 +1,11 @@
+import { task_base } from "../task_base";
+
+export class shop_equip extends task_base{
+    constructor(...a){
+        super(...a);
+        this.render();
+    }
+    async render(){
+        
+    }
+}
