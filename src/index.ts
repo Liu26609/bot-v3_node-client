9941c01820b12982884cfbe3a0a2f770c167d945
Liu26609 <70098755+Liu26609@users.noreply.github.com>
2022-06-12 02:00:14 +0800

    'autoUpdate'

--- src/index.ts
+++ src/index.ts
@@ -28,19 +28,19 @@ async function start() {
 async function test() {
     // log('匹配相识度', common.xsd('普通攻击(魔法', '普通攻击(魔法)'))
     // let data = await sever.callApi('Battle', {})
-    let image = `https://21-1257174510.cos.ap-nanjing.myqcloud.com/temp/equip.png?`;
-    // 装备图标
-    image += `imageMogr2/interlace/0/quality/75|watermark/1/image/${base64_safe.urlEncode(`http://21-1257174510.cos.ap-nanjing.myqcloud.com/equip/1.png`)}/dx/195/dy/220`;
-    // 称号
-    image += `imageMogr2/thumbnail/!50p|watermark/2/text/${base64_safe.urlEncode(`十米的大刀+10`)}/font/${base64_safe.urlEncode('simkai楷体.ttf')}/fill/${base64_safe.urlEncode(`#ffff00`)}/fontsize/32/dx/10/dy/390`;
-     // 品质
-     image += `imageMogr2/thumbnail/!50p|watermark/2/text/${base64_safe.urlEncode(`作者专属(武器)`)}/font/${base64_safe.urlEncode('simkai楷体.ttf')}/fill/${base64_safe.urlEncode(`#00ffff`)}/fontsize/24/dx/20/dy/350`;
-     // 属性
-     image += `imageMogr2/thumbnail/!50p|watermark/2/text/${base64_safe.urlEncode(`0
-1亿
-150万
-240十万
-5.万
-9998.0万`)}/font/${base64_safe.urlEncode('simkai楷体.ttf')}/fill/${base64_safe.urlEncode(`#ffffff`)}/fontsize/20/dx/10/dy/215`;
-    log('imgLink',image)
+//     let image = `https://21-1257174510.cos.ap-nanjing.myqcloud.com/temp/equip.png?`;
+//     // 装备图标
+//     image += `imageMogr2/interlace/0/quality/75|watermark/1/image/${base64_safe.urlEncode(`http://21-1257174510.cos.ap-nanjing.myqcloud.com/equip/1.png`)}/dx/195/dy/220`;
+//     // 称号
+//     image += `imageMogr2/thumbnail/!50p|watermark/2/text/${base64_safe.urlEncode(`十米的大刀+10`)}/font/${base64_safe.urlEncode('simkai楷体.ttf')}/fill/${base64_safe.urlEncode(`#ffff00`)}/fontsize/32/dx/10/dy/390`;
+//      // 品质
+//      image += `imageMogr2/thumbnail/!50p|watermark/2/text/${base64_safe.urlEncode(`作者专属(武器)`)}/font/${base64_safe.urlEncode('simkai楷体.ttf')}/fill/${base64_safe.urlEncode(`#00ffff`)}/fontsize/24/dx/20/dy/350`;
+//      // 属性
+//      image += `imageMogr2/thumbnail/!50p|watermark/2/text/${base64_safe.urlEncode(`0
+// 1亿
+// 150万
+// 240十万
+// 5.万
+// 9998.0万`)}/font/${base64_safe.urlEncode('simkai楷体.ttf')}/fill/${base64_safe.urlEncode(`#ffffff`)}/fontsize/20/dx/10/dy/215`;
+//     log('imgLink',image)
 }
