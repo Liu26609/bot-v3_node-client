6dfed6d85dcad7653b90602771c9e826b31b42e7
Liu26609 <70098755+Liu26609@users.noreply.github.com>
2022-06-14 22:54:40 +0800

    'autoUpdate'

--- Build/shared/shared/game/body.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bodyType = void 0;
-var bodyType;
-(function (bodyType) {
-    bodyType[bodyType["player"] = 0] = "player";
-    bodyType[bodyType["enemy"] = 1] = "enemy";
-})(bodyType = exports.bodyType || (exports.bodyType = {}));
