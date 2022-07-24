7abdebb53b12f17412a1f745cf8582fb1e5b12d9
Liu26609 <70098755+Liu26609@users.noreply.github.com>
2022-07-24 19:12:54 +0800

    'autoUpdate'

--- src/game/me/me_title.ts
+++ src/game/me/me_title.ts
@@ -4,13 +4,13 @@ import bot from '../../unity/bot';
 import sever from '../../unity/sever';
 import { task_base } from './../task_base';
 const gm = require('gm').subClass({ imageMagick: true })
-export class me_title extends task_base{
-    constructor(...a){
+export class me_title extends task_base {
+    constructor(...a) {
         super(...a);
         this.render();
     }
-    async render(){
-        let req = await sever.callApi('Me_title',{userId:this.userId});
+    async render() {
+        let req = await sever.callApi('Me_title', { userId: this.userId });
         if (!req.isSucc) {
             this.sendErr(req.err)
             return;
@@ -33,14 +33,15 @@ export class me_title extends task_base{
         // temp += `💖每秒回复+${data.title.attribute.secondResHp * leve}%\n`;
         // temp += `<emoji:147>攻击怪物/玩家可提高称号经验,等级越高加成越高\n`;
         // temp += `┗┄${this.at()}┄\n`;
-        // temp += `┏┄═══👑指令提示══━┄\n`
-        // temp += `🏷️称号改名[称号改名 + 名称]\n`
-        // temp += `🔂重置称号[重置称号 + 次数]\n`
-        // temp += `┗┄━═════════━┄`
         // bot.sendText(this.channel_id,temp,this.content)
-       this.renderImage(data)
+
+        let temp = `┏┄═══👑指令提示══━┄\n`
+        temp += `🏷️称号改名[称号改名 + 名称]\n`
+        temp += `🔂重置称号[重置称号 + 次数]\n`
+        temp += `┗┄━═════════━┄`
+        this.renderImage(data)
     }
-    renderImage(data:ResMe_title){
+    renderImage(data: ResMe_title) {
         const leve = data.title.leve;
         let time = Date.now()
         console.log('start')
@@ -53,23 +54,23 @@ export class me_title extends task_base{
         image.draw(`image Over 0,-160, 0, 0 "./image/title/1 (${common.random(52, 353)}).png"`)
         // NorthWest
         image.gravity('NorthWest')
-    
+
         image.fontSize(24)
         image.fill("#000000")
         image.fontSize(44)
         image.drawText(30, 75, `LV.${data.title.leve}`)
         image.draw(`image Over 21,142, 280, 6 "./image/exp_bar.png"`)
-        
+
 
         image.fontSize(12)
-        if(data.title.todayGetExp > 0){
+        if (data.title.todayGetExp > 0) {
             image.font('./font/MSungPRC-Xbold.ttf', 12)
             image.drawText(100, 138, `EXP:${data.title.exp}/${data.title.exp_max}`)
             image.font('./font/ttf_cn.ttf', 12)
-        }else{
+        } else {
             image.drawText(100, 138, 'EXP：今日称号可获得经验已达上限')
         }
-      
+
         image.fontSize(21)
         let startY = 202;
         let offset = 23;
@@ -79,17 +80,17 @@ export class me_title extends task_base{
         image.drawText(108, startY + offset * 3, `魔法攻击+${data.title.attribute.MagicAttack * leve}%`)
         image.drawText(108, startY + offset * 4, `魔法防御+${data.title.attribute.MagicDefense * leve}%`)
         image.drawText(108, startY + offset * 5, `生命回复+${data.title.attribute.secondResHp * leve}%`)
-       
+
         image.fontSize(12)
         image.gravity('SouthWest')
         image.drawText(15, 15, `By.${this.userName}`)
         let outId = Date.now()
         let outDir = `./out/${outId}.jpg`
 
-        image.write(outDir, (err)=> {
+        image.write(outDir, (err) => {
             console.log(`耗时${(Date.now() - time) / 1000}`)
             console.log('end')
-            bot.sendLocaImage(this.channel_id,outDir)
+            bot.sendLocaImage(this.channel_id, outDir)
         });
     }
 }
